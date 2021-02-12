68d2e1cd6ddda96f4df4d17d057e21663ab90e3c
Diego Palacios <diegoone>
2021-02-11 22:37:43 -0600

    agregados formatos soportados. Esta es la clase base

--- CadenaFormato.cs
+++ CadenaFormato.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Convertidor {
     class CadenaFormato {
+        const string FORMATO_MORSE = "MORSE";
+        const string FORMATO_TEXTO = "TEXTO";
+        const string FORMATO_BINARIO = "BINARIO";
         public CadenaFormato(string contenido, string formato) {
             if(formato != null && contenido!= null) {
                 this.contenido = contenido;
@@ -11,6 +14,6 @@ namespace Convertidor {
             }
         }
         public string formato {get;}
-        public string contenido {get; set;}
+        public string contenido {get;}
     }
 }
